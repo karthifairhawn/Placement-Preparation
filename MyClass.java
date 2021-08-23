6dee2c56f52bf2c65cc98e5c54925b6de162c746
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-08-23 13:20:52 +0530

    Insertion and Bubble Sort

---
+++ MyClass.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        int[] arr = {9,4,1,8,2,0,-1};
+        
+        int n = arr.length;
+        
+        
+        // Insertion Sort
+        // int key,j;
+        // for(int i=1;i<n;i++){
+        //     key=arr[i];
+        //     j=i-1;
+        //     while(j>=0 && arr[j]>key){
+        //         arr[j+1]=arr[j];
+        //         j--;
+        //     }
+        //     arr[j+1]=key;
+        // }
+        
+        // Bubble Sort
+        int temp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+          
+        System.out.println(Arrays.toString(arr));
+    }
+}
