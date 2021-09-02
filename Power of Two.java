4b24c12095807842728492aaae038919aa868bf3
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-09-02 22:05:43 +0530

    Power of 2

---
+++ Power of Two.java
@@ -0,0 +1,22 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if(n==2 || n==1) {
+            return true;
+        }
+        if(n%2==0) {
+            return isPowerOfTwo(n/2); 
+        }
+        
+        return false;
+    }
+}
