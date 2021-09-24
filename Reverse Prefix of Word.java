d706fb19647e10266556f2cf234d4d34d2018417
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-09-24 20:30:28 +0530

    Reverse Prefix of Word

---
+++ Reverse Prefix of Word.java
@@ -0,0 +1,20 @@
+// Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+// For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+// Return the resulting string.
+
+// Input: word = "abcdefd", ch = "d"
+// Output: "dcbaefd"
+// Explanation: The first occurrence of "d" is at index 3. 
+// Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+// Input: word = "xyxzxe", ch = "z"
+// Output: "zxyxxe"
+// Explanation: The first and only occurrence of "z" is at index 3.
+// Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+
+class Solution {
+    public String reversePrefix(String word, char ch) {            
+        return new StringBuilder(word.substring(0,word.indexOf(ch)+1)).reverse().toString() + word.substring(word.indexOf(ch)+1);
+    }
+}
