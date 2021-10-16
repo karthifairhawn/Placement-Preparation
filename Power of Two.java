6defef76c998d2ac7b9cd7c52baaf5fc5d69ca77
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-10-16 18:03:31 +0530

    Power of two

--- Power of Two.java
+++ Power of Two.java
@@ -8,15 +8,32 @@
 // Explanation: 20 = 1
 
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        if(n==2 || n==1) {
-            return true;
-        }
-        if(n%2==0) {
-            return isPowerOfTwo(n/2); 
+    //     public boolean isPowerOfTwo(int n) {
+    //         if(n<=0) return false;
+    //         if(n==2 || n==1) {
+    //             return true;
+    //         }
+    //         if(n%2==0) {
+    //             return isPowerOfTwo(n/2); 
+    //         }
+            
+    //         return false;
+    //     }
+        public boolean isPowerOfTwo(int n) {
+            
+            if(n<=0) return false;
+            
+            while(true){
+                if(n==1 || n==2) return true;
+                
+                if(n%2==0){
+                    n=n/2;
+                }else 
+                    break;
+            }
+            return false;
         }
         
-        return false;
+        
+        
     }
-}
