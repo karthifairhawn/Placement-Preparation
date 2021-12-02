2116b4cb4aec10312e34000e791321f0d9a0ede0
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-12-02 14:19:59 +0530

    4 Sorting algo completed

---
+++ Sorting Algorithmas/InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class InsertionSort
+{
+	public static void main(String[] args) {
+        int arr[] = {3,4,2,5,1};
+        int n = arr.length;
+        
+        
+        for(int i=1;i<n;i++){
+            int b = i-1;
+            int key = arr[i];
+            while(b>=0 && arr[b] > key){
+                arr[b+1] = arr[b];
+                b--;
+            }
+            arr[b+1] = key;
+        }
+        
+        
+        System.out.println(Arrays.toString(arr));
+	}
+}
