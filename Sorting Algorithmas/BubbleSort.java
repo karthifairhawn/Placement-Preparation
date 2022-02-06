d9b4ddc23f96007ef11e129913ba813c399040f3
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-02-06 19:53:15 +0530

    Infytq Question

--- Sorting Algorithmas/BubbleSort.java
+++ Sorting Algorithmas/BubbleSort.java
@@ -3,16 +3,21 @@ public class BubbleSort
 {
 	public static void main(String[] args) {
 		
-		int arr[] = {5,4,2,3,1};
+		int arr[] = {32,21,1,34,97,46,67,56};
 		int n = arr.length;
 		
 		for(int i=0;i<n;i++){
-		    for(int j=0;j<n-1-i;j++){
+			boolean swapped = false;
+		    for(int j=0;j<n-1-i;j++){				
 		        if(arr[j]>arr[j+1]){
+					swapped=true;
 					int temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;		            
 		        }
+				if(!swapped){
+					break;
+				}
 		    }
 		}
 		System.out.println(Arrays.toString(arr));
