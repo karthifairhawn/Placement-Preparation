5adb2fce4d2ab630d037dc62f6f28314ea6dc4c7
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-03-05 10:10:11 +0530

    Middle of linked list

--- Leet Code Solved Problems/Middle of the Linked List.java
+++ Leet Code Solved Problems/Middle of the Linked List.java
@@ -18,15 +18,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode[] A = new ListNode[100];
-        int t = 0;
-        while (head != null) {
-            A[t++] = head;
-            head = head.next;
-        }
-        return A[t / 2];
         
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        
+        if(fast.next!=null){
+            slow=slow.next;
+        }
+        return slow;
     }
 }
