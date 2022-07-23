e68b4e7f520929fd190778ddc6cd7a35b13d28e8
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-07-24 00:24:40 +0530

    PUT and Delete Calls

---
+++ API/ApiCallExample/PutCall.java
@@ -0,0 +1,27 @@
+package API.ApiCallExample;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PutCall {
+    public static void main(String[] args) throws IOException, InterruptedException {        
+      
+
+      HttpRequest req = HttpRequest.newBuilder()
+              .uri(URI.create("https://karthifairhawn.requestcatcher.com/"))
+              .PUT(HttpRequest.BodyPublishers.ofString("{\"name\":\"octocat\"}"))             
+              .build();
+      
+      HttpClient client = HttpClient.newHttpClient();
+
+      HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());     
+                               
+      System.out.println(response.body());
+            
+    }
+}
+
+
