5d556f3831eba7590272f8adbcddddf85efc1f7b
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-07-30 13:58:31 +0530

    Improved code readability Selection sort

--- SortingAlgorithmas/SelectionSort.java
+++ SortingAlgorithmas/SelectionSort.java
@@ -10,10 +10,11 @@ public class SelectionSort
 		
         for (int i = 0; i < n-1; i++){
             int small = i;
-            for (int j = i + 1; j < n; j++)
+            for (int j = i + 1; j < n; j++){
                 if (arr[j] < arr[small]){
                     small = j;
                 }
+            }
             int temp = arr[i];
             arr[i] = arr[small];
             arr[small] = temp;   	        
